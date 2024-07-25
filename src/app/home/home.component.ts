152448c5718af42bf72336ee23a2df44e38090ad
¨Omar <omarmaciasarevalo@gmail.com>
2024-07-24 21:23:36 -0500

    feat: mas ajustes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,31 +7,52 @@ import { HousingService } from '../housing.service';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,
+  imports: [
+    CommonModule,
     HousingLocationComponent
   ],
   template: `
-  <section>
-    <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
-    </form>
-  </section>
-  <section class="results">
-  <app-housing-location
-  *ngFor="let housingLocation of housingLocationList"
-  [housingLocation]="housingLocation">
-</app-housing-location>
-  </section>
-`,
-  styleUrl: './home.component.css'
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location
+        *ngFor="let housingLocation of filteredLocationList"
+        [housingLocation]="housingLocation">
+      </app-housing-location>
+    </section>
+  `,
+  styleUrls: ['./home.component.css'],
 })
 
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
-  HousingService: HousingService = inject(HousingService);
-  
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
+
   constructor() {
-    this.housingLocationList = this.HousingService.getAllHousingLocations();
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
+  
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(housingLocation => {
+      // Asegúrate de que housingLocation y housingLocation.city no sean nulos o indefinidos
+      if (housingLocation && housingLocation.city) {
+        // Convierte tanto city como text a minúsculas y verifica la inclusión
+        return housingLocation.city.toLowerCase().includes(text.toLowerCase());
+      }
+      return false; // Excluye las entradas nulas o indefinidas
+    });
   }
 }
